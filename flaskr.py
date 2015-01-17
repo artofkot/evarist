db7f6e1dac31b6cda863bc59d402c131ff00d679
Artem <artofkot@gmail.com>
2015-01-17 00:13:57 -0500

    init

---
+++ flaskr.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# all the imports. For small applications it’s a possibility to drop the configuration directly 
+# into the module which we will be doing here. However a cleaner solution would be 
+# to create a separate .ini or .py file and load that or import the values from there.
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash
+from contextlib import closing
+
+# configuration:
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+# !!!! Never leave debug mode activated in a production system
+SECRET_KEY = 'Shatahosik'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+# from_object() will look at the given object (if it’s a string it will import it) and 
+# then look for all uppercase variables defined there. In our case, the configuration we 
+# just wrote a few lines of code above. You can also move that into a separate file.
+
+# Usually, it is a good idea to load a configuration from a configurable file. 
+# This is what from_envvar() can do, replacing the from_object() line above:
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+# That way someone can set an environment variable called FLASKR_SETTINGS to specify a config file 
+# to be loaded which will then override the default values. The silent switch just tells 
+# Flask to not complain if no such environment key is set.
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE']) #DATABASE folder is set in config file
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None) # We use a neat trick here: if you use the pop() method of the dict and pass a second parameter to it (the default), the method will delete the key from the dictionary if present or do nothing when that key is not in there. This is helpful because now we don’t have to check if the user was logged in.
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.run()
