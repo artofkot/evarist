8f8c64a9f4011c0531be0bec91b3939d25663322
Artem <artofkot@gmail.com>
2015-01-18 11:40:18 -0500

    forgot to stage

--- flaskr.py
+++ flaskr.py
@@ -9,14 +9,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from contextlib import closing
 from flask.ext.pymongo import PyMongo
 
-# configuration:
-DEBUG = True
-# !!!! Never leave debug=True in a production system
-SECRET_KEY = 'Shatahosik'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-MONGO_URI="mongodb://heroku_app33294458:ohleelvsddqissik3r7nn74ge@ds031671.mongolab.com:31671/heroku_app33294458"
+# MONGO_URI="mongodb://heroku_app33294458:ohleelvsddqissik3r7nn74ge@ds031671.mongolab.com:31671/heroku_app33294458"
 # I generated this URI using service mongolab in heroku, see https://devcenter.heroku.com/articles/mongolab
 # locally on your computer when you install mongodb and then run it with command
 #   $mondod
@@ -25,15 +18,17 @@ MONGO_URI="mongodb://heroku_app33294458:ohleelvsddqissik3r7nn74ge@ds031671.mongo
 # then this the most important config variable MONGO_URI is equal to "mongodb://localhost:27017/"
 
 
-
-
 # create our little application :)
 app = Flask(__name__)
 
-# from_object() will look at the given object (if it’s a string it will import it) and 
-# then look for all uppercase variables defined there. In our case, the configuration we 
-# just wrote a few lines of code above. You can also move that into a separate file.
-app.config.from_object(__name__)
+# configuration
+app.config.update(dict(
+    MONGO_URI="mongodb://heroku_app33294458:ohleelvsddqissik3r7nn74ge@ds031671.mongolab.com:31671/heroku_app33294458",
+    DEBUG=True, # !!!! Never leave debug=True in a production system
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
 # Usually, it is a good idea to load a configuration from a configurable file. 
 # This is what from_envvar() can do, replacing the from_object() line above:
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
@@ -41,6 +36,27 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 # to be loaded which will then override the default values. The silent switch just tells 
 # Flask to not complain if no such environment key is set.
 
+
+
+# def connect_mongodb():
+#     """Connects to the specific database."""
+#     mongo = PyMongo(app)
+#     # PyMongo connects to the MongoDB server running on MONGO_URI, and assumes a default database name of app.name 
+#     # (i.e. whatever name you pass to Flask). This database is exposed as the db attribute.
+#     return mongo
+
+# def get_db():
+#     """Opens a new database connection if there is none yet for the
+#     current application context.
+#     """
+#     if not hasattr(g, 'mongodb'):
+#         g.mongodb = connect_mongodb()
+#     return g.mongodb
+
+# @app.teardown_appcontext
+# def close_db(error):
+#     """Closes the database again at the end of the request."""
+#     mongo.close()
 
 
 mongo = PyMongo(app)
@@ -89,7 +105,6 @@ def logout():
     session.pop('logged_in', None) # We use a neat trick here: if you use the pop() method of the dict and pass a second parameter to it (the default), the method will delete the key from the dictionary if present or do nothing when that key is not in there. This is helpful because now we don’t have to check if the user was logged in.
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-
 
 
 if __name__ == '__main__':
