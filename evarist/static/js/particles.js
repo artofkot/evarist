06d10a1684145eb6ed53e689ba8204c5df71eae7
Alexey Levin <alexej.levin@gmail.com>
2015-07-09 10:27:11 +0300

    minor

--- evarist/static/js/particles.js
+++ evarist/static/js/particles.js
@@ -1,10 +1,7 @@
-
 $(document).ready(function() {
   $('#particles').particleground({
     dotColor: 'rgb(225,225,225)',
     lineColor: 'rgb(225,225,225)',
-    // dotColor: '#eee',
-    // lineColor: '#eee',
     parallaxMultiplier: '3'
   });
 });
